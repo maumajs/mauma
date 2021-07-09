415357c4326369e4de193854e234f88acca17f4e
doup <asier@illarra.com>
2021-07-09 08:43:38 +0200

    Process generated HTML files /w Vite and copy non-HTML files

--- src/bin/build-vite.ts
+++ src/bin/build-vite.ts
@@ -1,4 +1,5 @@
 import globby from 'globby';
+import { copyFile } from 'fs/promises';
 import { join } from 'path';
 import { build } from 'vite';
 import { minifyHtml } from 'vite-plugin-html';
@@ -10,8 +11,20 @@ const prebuildDir = join(process.cwd(), '.mauma/build');
 const buildDir = join(process.cwd(), 'build');
 
 (async () => {
-  const files = await globby([join(prebuildDir, '**/*.html')]);
+  const files = await globby([join(prebuildDir, '**/*')]);
+  const filesOther: string[] = [];
+  const filesHTML: string[] = [];
 
+  // Separate HTML & non-HTML files
+  files.forEach(file => {
+    if (/\.html$/.test(file.toLowerCase())) {
+      filesHTML.push(file);
+    } else {
+      filesOther.push(file);
+    }
+  });
+
+  // Process HTML files /w Vite
   await build({
     root: prebuildDir,
     publicDir: publicDir,
@@ -33,11 +46,17 @@ const buildDir = join(process.cwd(), 'build');
       outDir: buildDir,
       emptyOutDir: true,
       rollupOptions: {
-        input: files,
+        input: filesHTML,
       }
     },
     plugins: [
       minifyHtml(),
     ],
   });
+
+  // Copy non-HTML files
+  await Promise.all(filesOther.map(file => {
+    const destFile = join(buildDir, file.replace(prebuildDir, ''));
+    return copyFile(file, destFile);
+  }));
 })();
